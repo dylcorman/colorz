57964517a2d54fcb444f80d646fcf74729c8abc5
LeifCastle <leifurcastle@gmail.com>
2023-07-11 18:28:49 -0700

    feat: basic Header component with link to home

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex items-center justify-center h-headerH bg-slate-700 text-white text-2xl">
+      <div className="basis-1/3">
+        <Link
+          href="/"
+          className="border-white border-2 rounded-md ml-10 text-base pl-1 pr-1"
+        >
+          Home
+        </Link>
+      </div>
+      <div className="basis-1/3 flex justify-center">
+        <h1>Colorz</h1>
+      </div>
+      <div className="basis-1/3"></div>
+    </div>
+  );
+}
